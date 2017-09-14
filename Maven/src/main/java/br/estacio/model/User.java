953ab364397a7df381d2efd8de47ee0e2a80de41
Carlos Alberto <allbertcarlo@gmail.com>
2017-09-14 13:42:39 -0300

    Projeto alterado

--- Maven/src/main/java/br/estacio/model/User.java
+++ Maven/src/main/java/br/estacio/model/User.java
@@ -1,26 +1,29 @@
 package br.estacio.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
 
+@Entity
 public class User {
 	
-
+	@Id
+	@GeneratedValue
 	private long id;
-	
-	
+
 	@Size(min = 10, message = "Entre pelo menos 10 caracteres")
 	private String name;
 	@Email
 	private String email;
-	@Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
-				, message = "Minimo 8 com maiúscula, minuscúlas, letras, números e caracteres." )
+	@Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Minimo 8 com maiúscula, minuscúlas, letras, números e caracteres.")
 	private String pwd;
-	
-	public void User(){
-		
+
+	public void User() {
+
 	}
 	
 	public long getId() {
